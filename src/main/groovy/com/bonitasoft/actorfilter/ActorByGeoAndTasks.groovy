3c00476a172965dbb0ab3f1fc1e182c1b7611dc8
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2021-03-31 11:59:38 +0200

    add business logic

--- src/main/groovy/com/bonitasoft/actorfilter/ActorByGeoAndTasks.groovy
+++ src/main/groovy/com/bonitasoft/actorfilter/ActorByGeoAndTasks.groovy
@@ -1,18 +1,28 @@
 package com.bonitasoft.actorfilter
 
-import org.bonitasoft.engine.api.APIAccessor
-import org.bonitasoft.engine.connector.ConnectorValidationException;
-import org.bonitasoft.engine.filter.AbstractUserFilter;
-import org.bonitasoft.engine.filter.UserFilterException;
-import org.bonitasoft.engine.identity.User;
-
 import groovy.util.logging.Slf4j
+import org.bonitasoft.engine.api.APIAccessor
+import org.bonitasoft.engine.connector.ConnectorValidationException
+import org.bonitasoft.engine.filter.AbstractUserFilter
+import org.bonitasoft.engine.filter.UserFilterException
+import org.bonitasoft.engine.identity.CustomUserInfoDefinition
+import org.bonitasoft.engine.identity.User
 
 @Slf4j
 class ActorByGeoAndTasks extends AbstractUserFilter {
-    
-    def static final MAXIMUM_WORKLOAD_INPUT = "maximumWorkload"
-    
+
+//    def static final MAXIMUM_WORKLOAD_INPUT = "customUserMaxTaskName"
+    def static final MAXIMUM_WORKLOAD_INPUT = "customUserMaxTaskName"
+    def static final GEO_ATTRIBUTE_NAME_INPUT = "customUserInfoGeoName"
+    def static final GEO_ATTRIBUTE_VALUE_INPUT = "customUserInfoGeoValue"
+    def static final AUTO_ASSIGN_INPUT = "autoAssign"
+
+    String maximumWorkload
+    String geoInputName
+    String geoInputValue
+    Boolean autoAssign
+
+
     /**
      * Perform validation on the inputs defined on the actorfilter definition (src/main/resources/actorByGeoAndTasks.def)
      * You should:
@@ -21,38 +31,51 @@ class ActorByGeoAndTasks extends AbstractUserFilter {
      */
     @Override
     def void validateInputParameters() throws ConnectorValidationException {
-        checkPositiveIntegerInput(MAXIMUM_WORKLOAD_INPUT)
+        validateStringInputParameterIsNotNulOrEmpty(MAXIMUM_WORKLOAD_INPUT)
+        validateStringInputParameterIsNotNulOrEmpty(GEO_ATTRIBUTE_NAME_INPUT)
+        validateStringInputParameterIsNotNulOrEmpty(GEO_ATTRIBUTE_VALUE_INPUT)
+
     }
 
-    def void checkPositiveIntegerInput(String inputName) throws ConnectorValidationException {
-        def value = getInputParameter(inputName)
-        if (value in Integer) {
-            if (!value || value <= 0) {
-                throw new ConnectorValidationException("Mandatory parameter '$inputName' must be a positive integer but is '$value'.")
-            }
-        } else {
-            throw new ConnectorValidationException("'$inputName' parameter must be an Integer")
-        }
-    }
-    
-    /**
-     * @return a list of {@link User} id that are the candidates to execute the task where this filter is defined.
-     * If the result contains a unique user, the task will automaticaly be assigned.
-     * @see AbstractUserFilter.shouldAutoAssignTaskIfSingleResult
-     */
     @Override
     def List<Long> filter(String actorName) throws UserFilterException {
-        def maximumWorkload = getInputParameter(MAXIMUM_WORKLOAD_INPUT)
+        maximumWorkload = getStringInputParameter(MAXIMUM_WORKLOAD_INPUT)
+        geoInputName = getStringInputParameter(GEO_ATTRIBUTE_NAME_INPUT)
+        geoInputValue = getStringInputParameter(GEO_ATTRIBUTE_VALUE_INPUT)
+        geoInputValue = getStringInputParameter(GEO_ATTRIBUTE_VALUE_INPUT)
+        autoAssign = getInputParameter(AUTO_ASSIGN_INPUT).toString().toBoolean()
+
+        log.info "maximumWorkload = ${maximumWorkload}"
+        log.info "geoInputName = ${geoInputName}"
+        log.info "geoInputValue = ${geoInputValue}"
+        log.info "autoAssign = ${autoAssign}"
+
         def apiAccessor = getAPIAccessor()
         def processAPI = apiAccessor.getProcessAPI()
         def users = processAPI.getUserIdsForActor(getExecutionContext().getProcessDefinitionId(), actorName, 0, Integer.MAX_VALUE);
 
-        log.info "${MAXIMUM_WORKLOAD_INPUT} input = ${maximumWorkload}"
-        users.findAll { !isOverloaded(it, apiAccessor, maximumWorkload as Integer) }
+        List<CustomUserInfoDefinition> customUserInfoDefinitions = apiAccessor.getIdentityAPI().getCustomUserInfoDefinitions(0, 10)
+        def geoDefinition = customUserInfoDefinitions.find { it.name == geoInputName }
+        def maxTasksDefinition = customUserInfoDefinitions.find { it.name == maximumWorkload }
+
+        users.findAll { isCandidate(it, apiAccessor, geoDefinition, maxTasksDefinition) }
     }
 
+    boolean isCandidate(Long userId, APIAccessor apiAccessor, geoDefinition, maxTasksDefinition) {
+        def customUserInfos = apiAccessor.getIdentityAPI().getCustomUserInfo(userId, 0, 10)
+        def geoAttribute = customUserInfos.find { (it.getDefinition().id == geoDefinition.id) }
+        def maxTaskAttribute = customUserInfos.find { (it.getDefinition().id == maxTasksDefinition.id) }
 
-    def boolean isOverloaded(Long userId, APIAccessor apiAccessor, int maximumWorkload) {
-        apiAccessor.getProcessAPI().getNumberOfAssignedHumanTaskInstances(userId) >= maximumWorkload
+        long maxTasks = maxTaskAttribute?.value?.toLong()
+        long assigned = apiAccessor.getProcessAPI().getNumberOfAssignedHumanTaskInstances(userId)
+        def candidate = (assigned < maxTasks) && ((geoAttribute?.value) == geoInputValue)
+
+        log.debug "user ${userId} | assigned: ${assigned} | max: ${maxTasks} | geoAttribute: ${geoAttribute?.value} | candidate:${candidate}"
+        candidate
+    }
+
+    @Override
+    boolean shouldAutoAssignTaskIfSingleResult() {
+        return autoAssign
     }
 }
--- src/test/groovy/com/bonitasoft/actorfilter/ActorByGeoAndTasksTest.groovy
+++ src/test/groovy/com/bonitasoft/actorfilter/ActorByGeoAndTasksTest.groovy
@@ -1,22 +1,26 @@
 package com.bonitasoft.actorfilter
 
 import org.bonitasoft.engine.api.APIAccessor
+import org.bonitasoft.engine.api.IdentityAPI
 import org.bonitasoft.engine.api.ProcessAPI
 import org.bonitasoft.engine.connector.ConnectorValidationException
 import org.bonitasoft.engine.connector.EngineExecutionContext
-
+import org.bonitasoft.engine.identity.CustomUserInfo
+import org.bonitasoft.engine.identity.impl.CustomUserInfoDefinitionImpl
+import org.bonitasoft.engine.identity.impl.CustomUserInfoValueImpl
 import spock.lang.Specification
-
 
 class ActorByGeoAndTasksTest extends Specification {
 
     def ActorByGeoAndTasks filter
     APIAccessor apiAccessor = Mock(APIAccessor)
     ProcessAPI processApi = Mock(ProcessAPI)
+    IdentityAPI identityAPI = Mock(IdentityAPI)
     EngineExecutionContext engineExecutionContext = Mock(EngineExecutionContext)
 
     def setup() {
         apiAccessor.getProcessAPI() >> processApi
+        apiAccessor.getIdentityAPI() >> identityAPI
         filter = new ActorByGeoAndTasks()
         filter.setAPIAccessor(apiAccessor)
         filter.setExecutionContext(engineExecutionContext)
@@ -33,44 +37,55 @@ class ActorByGeoAndTasksTest extends Specification {
         thrown ConnectorValidationException
     }
 
-    def should_throw_exception_if_mandatory_input_is_not_positive_integer() {
-        given: 'An actorfilter with a negative input'
-        filter.setInputParameters([(ActorByGeoAndTasks.MAXIMUM_WORKLOAD_INPUT):-1])
-
-        when: 'Validating inputs'
-        filter.validateInputParameters()
-
-        then: 'ConnectorValidationException is thrown'
-        thrown ConnectorValidationException
-    }
-
-    def should_throw_exception_if_mandatory_input_is_not_an_integer() {
-        given: 'An actorfilter with a String input'
-        filter.setInputParameters([(ActorByGeoAndTasks.MAXIMUM_WORKLOAD_INPUT):'1'])
-
-        when: 'Validating inputs'
-        filter.validateInputParameters()
-
-        then: 'ConnectorValidationException is thrown'
-        thrown ConnectorValidationException
-    }
-
     def should_return_a_list_of_candidates() {
         given: 'Users with some task already assigned'
         processApi.getUserIdsForActor(_ as Long, 'MyActor', 0, Integer.MAX_VALUE) >> [1L, 2L, 3L]
-        processApi.getNumberOfAssignedHumanTaskInstances(1L) >> 2L
-        processApi.getNumberOfAssignedHumanTaskInstances(2L) >> 3L
-        processApi.getNumberOfAssignedHumanTaskInstances(3L) >> 0L
+        processApi.getNumberOfAssignedHumanTaskInstances(1L) >> 20L
+        processApi.getNumberOfAssignedHumanTaskInstances(2L) >> 9L
+        processApi.getNumberOfAssignedHumanTaskInstances(3L) >> 7L
+
+        def geoDef = new CustomUserInfoDefinitionImpl(id: 456L, name: 'geo', description: null)
+        def maxDef = new CustomUserInfoDefinitionImpl(id: 789L, name: 'maxTasks', description: null)
+        def otherDef = new CustomUserInfoDefinitionImpl(id: 743L, name: 'other', description: null)
+
+        apiAccessor.getIdentityAPI().getCustomUserInfoDefinitions(_, _) >> [geoDef, maxDef, otherDef]
+
+        identityAPI.getCustomUserInfo(1L, _, _) >> {
+            def argumentUserId = 1L
+            [new CustomUserInfo(argumentUserId, geoDef,
+                    new CustomUserInfoValueImpl(userId: argumentUserId, value: 'Africa', definitionId: geoDef.id)),
+             new CustomUserInfo(argumentUserId, maxDef,
+                     new CustomUserInfoValueImpl(userId: argumentUserId, value: '35', definitionId: maxDef.id)),]
+        }
+
+        identityAPI.getCustomUserInfo(2L, _, _) >> {
+            def argumentUserId = 2L
+            [new CustomUserInfo(argumentUserId, geoDef,
+                    new CustomUserInfoValueImpl(userId: argumentUserId, value: 'Japan', definitionId: geoDef.id)),
+             new CustomUserInfo(argumentUserId, maxDef,
+                     new CustomUserInfoValueImpl(userId: argumentUserId, value: '12', definitionId: maxDef.id)),]
+        }
+        identityAPI.getCustomUserInfo(3L, _, _) >> {
+            def argumentUserId = 3L
+            [new CustomUserInfo(argumentUserId, geoDef,
+                    new CustomUserInfoValueImpl(userId: argumentUserId, value: 'Japan', definitionId: geoDef.id)),
+             new CustomUserInfo(argumentUserId, maxDef,
+                     new CustomUserInfoValueImpl(userId: argumentUserId, value: '7', definitionId: maxDef.id)),]
+        }
 
         and: 'An actor filter with a valid maximum workload'
-        filter.setInputParameters([(ActorByGeoAndTasks.MAXIMUM_WORKLOAD_INPUT):'3'])
+        filter.setInputParameters([
+                (ActorByGeoAndTasks.MAXIMUM_WORKLOAD_INPUT)   : 'maxTasks',
+                (ActorByGeoAndTasks.GEO_ATTRIBUTE_NAME_INPUT) : 'geo',
+                (ActorByGeoAndTasks.GEO_ATTRIBUTE_VALUE_INPUT): 'Japan',
+        ])
 
         when: 'Applying filter to the existing users'
-        def candidates = filter.filter("MyActor");
+        filter.validateInputParameters()
+        def candidates = filter.filter("MyActor")
 
         then: 'Only users with a workload below the maximum are returned as candidates'
-        assert candidates.contains(1L)
-        assert candidates.contains(3L)
-        assert !candidates.contains(2L)
+        candidates == [2L]
+
     }
 }
